f9b6f6170c18ccd997216f6d15d3c9b5c034abc4
Mobiuus-learn <igorsantos.2116@gmail.com>
2022-07-23 10:41:52 -0300

    adicionado visualização de grupos

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 // @Criado por José Rodrigo
 // @Contribuição de Ubiratan Lima
 // @2022
+let sp = document.createElement('span')
+let textGroup = document.createElement('span')
 
+const btnCalc = document.querySelector('#btn-calcColor')
+const inputFiber = document.querySelector("#in-fiber")
+const colorReturn = document.querySelector("#color-return")
+const classReset = document.querySelector('#class-reset')
+let textColor = document.querySelector('.text-color')
+let groupColor = document.querySelector("#group-color")
+let groupReturn = document.querySelector('.group-return')
+let fiberColor
 
 
 const cores = [
@@ -18,58 +28,92 @@ const cores = [
     { cor: 'laranja', fibra: [11, 23, 35, 47, 59, 71, 83, 95, 107, 119, 131, 143] },
     { cor: 'aqua', fibra: [12, 24, 36, 48, 60, 72, 84, 96, 108, 120, 132, 144] }
 ]
-const btnCalc = document.querySelector('#btn-calcColor')
-const inputFiber = document.querySelector("#in-fiber")
-const colorReturn = document.querySelector("#color-return")
-const classReset = document.querySelector('#class-reset')
-let textColor = document.querySelector('.text-color')
-let sp = document.createElement('span')
-let groupColor = document.querySelector(".group-color")
-let fiberColor
 
-const groupID = [
-    { cor: "verde", grupo: 1 },
-    { cor: "amarelo", grupo: 2 },
-    { cor: "branco", grupo: 3 },
-    { cor: "azul", grupo: 4 },
-    { cor: "vermelho", grupo: 5 },
-    { cor: "violeta", grupo: 6 },
-    { cor: "marron", grupo: 7 },
-    { cor: "rosa", grupo: 8 },
-    { cor: "preto", grupo: 9 },
-    { cor: "cinza", grupo: 10 },
-    { cor: "laranja", grupo: 11 },
-    { cor: "aqua", grupo: 12 }
-]
+
+
+
 btnCalc.addEventListener('click', function () {
     fiberColor = inputFiber.value
-
-
     for (item in cores) {
         for (index in cores[item].fibra) {
             if (cores[item].fibra[index] == fiberColor) {
                 textColor.appendChild(sp)
                 sp.innerHTML = ` ${cores[item].cor}`
                 colorReturn.classList.add(cores[item].cor)
-                // // console.log(el)
-                // if (index == 0) {
-                //     console.log('verde', index)
-                //     groupColor.classList.add(groupID[0].cor)
-                // }
-                // if (index == 1) {
-                //     console.log('amarelo', index)
-                //     groupColor.classList.add(groupID[1].cor)
-                // }
+                if (cores[item].fibra[index] >= 1 && cores[item].fibra[index] <= 12) {
+                    groupColor.classList.add("verde")
+                    // let textGroup = document.createElement('span')
+                    textGroup.innerHTML = 'verde'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 13 && cores[item].fibra[index] <= 24) {
+                    groupColor.classList.add("amarelo")
+                    textGroup.innerHTML = 'amarelo'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 25 && cores[item].fibra[index] <= 36) {
+                    groupColor.classList.add("branco")
+                    textGroup.innerHTML = 'branco'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 37 && cores[item].fibra[index] <= 48) {
+                    groupColor.classList.add("azul")
+                    textGroup.innerHTML = 'azul'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 49 && cores[item].fibra[index] <= 60) {
+                    groupColor.classList.add("vermelho")
+                    textGroup.innerHTML = 'vermelho'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 61 && cores[item].fibra[index] <= 72) {
+                    groupColor.classList.add("violeta")
+                    textGroup.innerHTML = 'violeta'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 73 && cores[item].fibra[index] <= 84) {
+                    groupColor.classList.add("marrom")
+                    textGroup.innerHTML = 'marrom'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 85 && cores[item].fibra[index] <= 96) {
+                    groupColor.classList.add("rosa")
+                    textGroup.innerHTML = 'rosa'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 97 && cores[item].fibra[index] <= 108) {
+                    groupColor.classList.add("preto")
+                    textGroup.innerHTML = 'preto'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 109 && cores[item].fibra[index] <= 120) {
+                    groupColor.classList.add("cinza")
+                    textGroup.innerHTML = 'cinza'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 121 && cores[item].fibra[index] <= 132) {
+                    groupColor.classList.add("laranja")
+                    textGroup.innerHTML = 'laranja'
+                    groupReturn.appendChild(textGroup)
+                }
+                if (cores[item].fibra[index] >= 133 && cores[item].fibra[index] <= 144) {
+                    groupColor.classList.add("aqua")
+                    textGroup.innerHTML = 'aqua'
+                    groupReturn.appendChild(textGroup)
+                }
             }
-
         }
+
+
     }
 }
-
 )
+
+
 
 classReset.addEventListener('click', function () {
     colorReturn.removeAttribute('class')
+    groupColor.removeAttribute('class')
     inputFiber.value = ""
     sp.innerHTML = ""
 
