93ed7b327e5cbb7917fcfae9df1bb65dd5731332
Mobiuus-learn <igorsantos.2116@gmail.com>
2022-07-30 03:27:00 -0300

    versao beta

--- scripts/36.js
+++ scripts/36.js
@@ -17,6 +17,15 @@ let groupReturn = document.querySelector('.group-return')
 let fiberColor
 
 
+let verde = { menor: 1, maior: 139 }
+let amarelo = { menor: 2, maior: 140 }
+let branco = { menor: 3, maior: 141 }
+let azul = { menor: 4, maior: 142 }
+let vermelho = { menor: 5, maior: 143 }
+let violeta = { menor: 0, maior: 0 }
+
+
+
 
 
 
@@ -24,6 +33,8 @@ btnCalc.addEventListener('click', function () {
     fiberColor = inputFiber.value
     let fiber = fiberColor % 6
 
+
+    // check fiber color
     switch (fiber) {
         case 1:
             colorReturn.classList.add('verde')
@@ -55,14 +66,43 @@ btnCalc.addEventListener('click', function () {
             textColor.appendChild(sp)
             sp.innerHTML = "violeta"
             break;
-
-
-
         default:
             break;
     }
 
+    if (fiber >= verde.menor && fiber <= verde.maior) {
+        groupColor.classList.add('verde')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'verde'
+    }
+    if (fiber >= amarelo.menor && fiber <= amarelo.maior) {
+        groupColor.classList.add('amarelo')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'amarelo'
+    }
+    if (fiber >= branco.menor && fiber <= branco.maior) {
+        groupColor.classList.add('branco')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'branco'
+    }
+    if (fiber >= azul.menor && fiber <= azul.maior) {
+        groupColor.classList.add('azul')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'azul'
+    }
+    if (fiber >= vermelho.menor && fiber <= vermelho.maior) {
+        groupColor.classList.add('vermelho')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'vermelho'
+    }
+    if (fiber == 0) {
+        groupColor.classList.add('violeta')
+        groupReturn.appendChild(textGroup)
+        textGroup.innerHTML = 'violeta'
+    }
+
 }
+
 )
 
 
